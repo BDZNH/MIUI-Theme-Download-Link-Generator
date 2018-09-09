5982403e5e1a2ab1efae388a3bc20449eb1f1399
BDZNH <pxezhsp@live.com>
2018-09-09 17:17:36 +0800

    布局调整

---
+++ MIUI_Theme_Download_Link_Generator/generate.cpp
@@ -0,0 +1,85 @@
+#include "generate.h"
+
+bool Genarate(std::string Geturl, char * tureUrl)
+{
+
+	std::string url("http://thm.market.xiaomi.com/thm/download/v2/");
+	const std::string headOfUrl2("http://zhuti.xiaomi.com/detail/");
+
+	if (Geturl.size() <= url.size())
+	{
+		strcpy(tureUrl, u8"错误的链接");
+		return false;
+	}
+
+	url += Geturl.substr(headOfUrl2.size(), Geturl.size() - headOfUrl2.size());
+
+
+	CURL* curl = curl_easy_init();
+
+	// Set remote URL.
+	curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+	// Don't bother trying IPv6, which would increase DNS resolution time.
+	curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+
+	// Don't wait forever, time out after 10 seconds.
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10);
+
+	// Follow HTTP redirects if necessary.
+	curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+	// Response information.
+	int httpCode(0);
+	std::unique_ptr<std::string> httpData(new std::string());
+
+	// Hook up data handling function.
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+
+	// Hook up data container (will be passed as the last parameter to the
+	// callback handling function).  Can be any pointer type, since it will
+	// internally be passed as a void pointer.
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData.get());
+
+	// Run our HTTP GET command, capture the HTTP response code, and clean up.
+	curl_easy_perform(curl);
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+	curl_easy_cleanup(curl);
+
+	if (httpCode == 200)
+	{
+
+		Json::Value jsonData;
+		Json::Reader jsonReader;
+
+		if (jsonReader.parse(*httpData, jsonData))
+		{
+
+			std::string Json(jsonData.toStyledString());
+
+			//use regex to find the direct_download_link.
+			/*std::string pattern("http(s)?://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?");*/
+			std::string pattern("(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]");
+			pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+			std::regex r(pattern);
+			std::sregex_iterator it(Json.begin(), Json.end(), r);
+
+			std::string temp(it->str());
+			strcpy(tureUrl, temp.c_str());
+
+		}
+		else
+		{
+			strcpy(tureUrl, u8"生成链接失败");
+			return false;
+		}
+	}
+	else
+	{
+		//std::cout << "Couldn't GET from " << url << " - exiting" << std::endl;
+		strcpy(tureUrl, u8"生成链接失败");
+		return false;
+	}
+
+	return true;
+}
--- MIUI_Theme_Download_Link_Generator/main.cpp
+++ MIUI_Theme_Download_Link_Generator/main.cpp
@@ -1,139 +1,252 @@
-#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"") //remove cmd window.
-#define _CRT_SECURE_NO_WARNINGS
+Ôªø// ImGui - standalone example application for DirectX 11
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
 
-#include "generate.h" //generate download link.
-#include <imgui/imgui.h>
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl2.h"
-#include <stdio.h>
-#include <GLFW/glfw3.h>
-#include <cstring>
+#include "../include/imgui/imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_dx11.h"
+#include <d3d11.h>
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#include <tchar.h>
 #include <Windows.h>
+#include "resource.h"
 
+#include "generate.h"
 
-static void glfw_error_callback(int error, const char* description)
+// Data
+static ID3D11Device*            g_pd3dDevice = NULL;
+static ID3D11DeviceContext*     g_pd3dDeviceContext = NULL;
+static IDXGISwapChain*          g_pSwapChain = NULL;
+static ID3D11RenderTargetView*  g_mainRenderTargetView = NULL;
+
+void CreateRenderTarget()
 {
-	fprintf(stderr, "Glfw Error %d: %s\n", error, description);
+	ID3D11Texture2D* pBackBuffer;
+	g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_mainRenderTargetView);
+	pBackBuffer->Release();
 }
 
-int main()
+void CleanupRenderTarget()
 {
-	// Setup window
-	glfwSetErrorCallback(glfw_error_callback);
-	if (!glfwInit())
+	if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = NULL; }
+}
+
+HRESULT CreateDeviceD3D(HWND hWnd)
+{
+	// Setup swap chain
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 2;
+	sd.BufferDesc.Width = 0;
+	sd.BufferDesc.Height = 0;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	UINT createDeviceFlags = 0;
+	//createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+	D3D_FEATURE_LEVEL featureLevel;
+	const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
+	if (D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext) != S_OK)
+		return E_FAIL;
+
+	CreateRenderTarget();
+
+	return S_OK;
+}
+
+void CleanupDeviceD3D()
+{
+	CleanupRenderTarget();
+	if (g_pSwapChain) { g_pSwapChain->Release(); g_pSwapChain = NULL; }
+	if (g_pd3dDeviceContext) { g_pd3dDeviceContext->Release(); g_pd3dDeviceContext = NULL; }
+	if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = NULL; }
+}
+
+extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+		return true;
+
+	switch (msg)
+	{
+	case WM_SIZE:
+		if (g_pd3dDevice != NULL && wParam != SIZE_MINIMIZED)
+		{
+			ImGui_ImplDX11_InvalidateDeviceObjects();
+			CleanupRenderTarget();
+			g_pSwapChain->ResizeBuffers(0, (UINT)LOWORD(lParam), (UINT)HIWORD(lParam), DXGI_FORMAT_UNKNOWN, 0);
+			CreateRenderTarget();
+			ImGui_ImplDX11_CreateDeviceObjects();
+		}
+		return 0;
+	case WM_SYSCOMMAND:
+		if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+			return 0;
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+	// Create application window
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, _T("ImGui Example"), NULL };
+	wc.hIcon= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	wc.hIconSm = LoadIcon(wc.hInstance, MAKEINTRESOURCE(IDI_ICON1));
+	RegisterClassEx(&wc);
+	HWND hwnd = CreateWindow(_T("ImGui Example"), _T("MIUI ‰∏ªÈ¢ò‰∏ãËΩΩÈìæÊé•ÁîüÊàê"),
+		WS_SYSMENU | \
+		WS_MINIMIZEBOX, 100, 100, 1018, 165, NULL, NULL, wc.hInstance, NULL);
+
+	// Initialize Direct3D
+	if (CreateDeviceD3D(hwnd) < 0)
+	{
+		CleanupDeviceD3D();
+		UnregisterClass(_T("ImGui Example"), wc.hInstance);
 		return 1;
-	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-	GLFWwindow* window = glfwCreateWindow(1000, 140, u8"–°√◊÷˜Ã‚œ¬‘ÿ¡¥Ω”…˙≥…", NULL, NULL);
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1); // Enable vsync
+	}
+
+	// Show the window
+	ShowWindow(hwnd, SW_SHOWDEFAULT);
+	UpdateWindow(hwnd);
 
 	// Setup Dear ImGui binding
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
-	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
 
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL2_Init();
+	ImGui_ImplWin32_Init(hwnd);
+	ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
 
 	// Setup style
 	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
 
 	// Load Fonts
-	ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\msyh.ttc", 18.0f, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
 
-	
+	ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\msyh.ttc", 18.0f, NULL, io.Fonts->GetGlyphRangesChineseSimplifiedCommon());
+	//IM_ASSERT(font != NULL);
+
+
 	bool show_main_window = true;
-	bool output_Text_box = false;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 	// Main loop
-	while (!glfwWindowShouldClose(window))
+	MSG msg;
+	ZeroMemory(&msg, sizeof(msg));
+	while (msg.message != WM_QUIT)
 	{
-		// Poll and handle events (inputs, window resize, etc.)
+		// Poll and handle messages (inputs, window resize, etc.)
 		// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
 		// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
 		// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
 		// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
-		glfwPollEvents();
+		if (PeekMessage(&msg, NULL, 0U, 0U, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			continue;
+		}
 
 		// Start the ImGui frame
-
-		ImGui_ImplOpenGL2_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
+		ImGui_ImplDX11_NewFrame();
+		ImGui_ImplWin32_NewFrame();
 		ImGui::NewFrame();
 
-		//Set the property of the window.
+
 		ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_FirstUseEver);
-		ImGui::SetNextWindowSize(ImVec2(1000, 140), ImGuiCond_FirstUseEver);
+		ImGui::SetNextWindowSize(ImVec2(1000, 125), ImGuiCond_FirstUseEver);
 		ImGui::Begin("Main Window", &show_main_window, ImGuiWindowFlags_NoTitleBar \
-			| ImGuiWindowFlags_NoMove \
 			| ImGuiWindowFlags_NoResize \
-			| ImGuiWindowFlags_ShowBorders \
 			| ImGuiWindowFlags_NoMove);
-		
-		
-		static char elevenBytes[1000] = u8"–°√◊÷˜Ã‚µƒ¡¥Ω”";
-		static char textShowed[1000] = u8"∏¥÷∆œ¬‘ÿ¡¥Ω”";
-		static char author[100] = u8"https://github.com/BDZNH/MIUI-Theme-Download-Link-Generator";
-		static float f = 0.0f;
 
-		//ImGui::PushItemWidth(100);
-		ImGui::Text(u8"’≥Ã˘–°√◊÷˜Ã‚µƒ¡¥Ω”"); 
-		//ImGui::PopItemWidth();
-		ImGui::SameLine();
-		ImGui::InputText("--", elevenBytes, IM_ARRAYSIZE(elevenBytes));
-		ImGui::SameLine();
-		
-		if (ImGui::Button(u8"…˙≥…"))
+		// 1. Show a simple window.
+		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets automatically appears in a window called "Debug".
 		{
-			output_Text_box = true;
+
+			ImGui::Text(u8"Áî®Ê≥ïÔºöÁ≤òË¥¥Â∞èÁ±≥‰∏ªÈ¢òÁöÑÈìæÊé•Âà∞Á¨¨‰∏Ä‰∏™Ê°ÜÔºåÁÇπÂáªÁîüÊàê‰∏ãËΩΩÈìæÊé•ÔºåÁÑ∂ÂêéÁÇπÂáªÂ§çÂà∂ÊàñËÄÖ‰∏ãËΩΩ„ÄÇby BDZNH");
+			static char link[128] = u8"Á≤òË¥¥Â∞èÁ±≥‰∏ªÈ¢òÁöÑÈìæÊé•";
+			static char direct_link[128] = u8"I'm here";
+			ImGui::PushItemWidth(ImGui::GetWindowWidth() - 120);
+			ImGui::InputText(u8"Â∞èÁ±≥‰∏ªÈ¢òÁöÑÈìæÊé•", link, IM_ARRAYSIZE(link));
+			ImGui::InputText(u8"ÁîüÊàêÁöÑ‰∏ªÈ¢òÈìæÊé•", direct_link, IM_ARRAYSIZE(direct_link));
+			ImGui::PopItemWidth();
+			ImGui::PushItemWidth(120);
+			if (ImGui::Button(u8"ÁîüÊàê‰∏ãËΩΩÈìæÊé•"))
+			{
+				Genarate(link, direct_link);
+			}
+			ImGui::SameLine();
+
+			//copy the direct download link to clipboard.
+			if (ImGui::Button(u8"Â§çÂà∂"))
+			{
+				Genarate(link, direct_link);
+				ImGui::LogToClipboard();
+				ImGui::LogText(direct_link);
+				ImGui::LogFinish();
+			}
+			ImGui::SameLine();
+
+			//open the default browser and download the theme.
+			if (ImGui::Button(u8"‰∏ãËΩΩ"))
+			{
+				Genarate(link, direct_link);
+				ShellExecute(NULL, _T("open"), _T("explorer.exe"), _T(direct_link), NULL, SW_SHOW);
+			}
+			ImGui::SameLine();
+
+			//access me.
+			if (ImGui::Button(u8"ËÅîÁ≥ª‰ΩúËÄÖ"))
+			{
+				ShellExecute(NULL, _T("open"), _T("explorer.exe"), _T("https://github.com/BDZNH/MIUI-Theme-Download-Link-Generator"), NULL, SW_SHOW);
+			}
+			ImGui::SameLine();
+
+			ImGui::Text("https://github.com/BDZNH/MIUI-Theme-Download-Link-Generator");
+			ImGui::SameLine();
+
 		}
-		/*ImGui::Text("hahaha");*/
-		if (output_Text_box)
-		{
-			//strcpy(textShowed, elevenBytes);
-			Genarate(elevenBytes, textShowed);
-			output_Text_box = false;
-		}
-		ImGui::Text(u8"…˙≥…µƒ–°√◊÷˜Ã‚¡¥Ω”"); ImGui::SameLine();
-		ImGui::InputText("~", textShowed, IM_ARRAYSIZE(textShowed));
-
-		//ImGui::SameLine();
-
-		//if (ImGui::Button(u8"∏¥÷∆"))
-		//{
-		//	//tocliboard(textShowed);
-		//	tocliboard();
-		//}
-
-		ImGui::Text(u8" π”√Àµ√˜£∫∞—–°√◊÷˜Ã‚µƒ¡¥Ω”’≥Ã˘µΩµ⁄“ª∏ˆøÚƒ⁄£¨»ª∫Ûµ„ª˜\"…˙≥…\"£¨÷˜Ã‚µƒœ¬‘ÿ¡¥Ω”ª·…˙≥…µΩµ⁄∂˛∏ˆøÚƒ⁄£¨»ª∫Ûµ„µ⁄“ªœ¬µ⁄∂˛∏ˆøÚ≤¢∞¥\"ctrl A\"£¨");
-		ImGui::Text(u8"»ª∫Û∞¥\"ctrl c\"∏¥÷∆œ¬‘ÿ¡¥Ω”£¨µΩChromeªÚ’ﬂIEµƒµÿ÷∑¿∏’≥Ã˘£¨ªÿ≥µº¥ø…œ¬‘ÿ°£");
-		ImGui::Text(u8"¡™œµ◊˜’ﬂ:  "); ImGui::SameLine();
-		ImGui::InputText(" ", author, IM_ARRAYSIZE(author));
 
 		ImGui::End();
-		ImGui::Render();
-		int display_w, display_h;
-		glfwGetFramebufferSize(window, &display_w, &display_h);
-		glViewport(0, 0, display_w, display_h);
-		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-		glClear(GL_COLOR_BUFFER_BIT);
-		//glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound, but prefer using the GL3+ code.
-		ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
 
-		glfwMakeContextCurrent(window);
-		glfwSwapBuffers(window);
+		// Rendering
+		ImGui::Render();
+		g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, NULL);
+		g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, (float*)&clear_color);
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+		g_pSwapChain->Present(1, 0); // Present with vsync
+		//g_pSwapChain->Present(0, 0); // Present without vsync
 	}
 
-	// Cleanup
-	ImGui_ImplOpenGL2_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
 	ImGui::DestroyContext();
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
+	CleanupDeviceD3D();
+	DestroyWindow(hwnd);
+	UnregisterClass(_T("ImGui Example"), wc.hInstance);
 
 	return 0;
 }
